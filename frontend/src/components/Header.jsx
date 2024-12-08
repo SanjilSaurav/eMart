528ed7c54bb428cd5ac649a72322f943e8ed1c8c
Saurav Kumar <saurav.0kmr@gmail.com>
2024-12-08 19:38:03 +0530

    Cart Screen and functionality to add to cart

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,12 @@
-import{ Navbar, Nav, Container } from 'react-bootstrap';
+import{ Badge, Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import { LinkContainer } from 'react-router-bootstrap';
+import { useSelector } from 'react-redux';
 import logo from '../assets/logo.png';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+    const { cartItems } = useSelector((state) => state.cart);
   return (
     <header>
         <Navbar bg="dark" variant='dark' expand='lg' collapseOnSelect>
@@ -19,7 +21,13 @@ const Header = () => {
                 <Navbar.Collapse id='basic-navbar-nav'>
                     <Nav className='ms-auto'>
                         {/* <LinkContainer to='/cart'> */}
-                            <Nav.Link as={Link} to='/cart'><FaShoppingCart/>Cart</Nav.Link>
+                            <Nav.Link as={Link} to='/cart'><FaShoppingCart/>Cart{
+                                cartItems.length > 0 && (
+                                    <Badge pill bg='success' style={{marginLeft: '5px'}}>
+                                        { cartItems.reduce((a, c) => a+c.qty, 0)}
+                                    </Badge>
+                                )
+                            }</Nav.Link>
                         {/* </LinkContainer> */}
                         {/* <LinkContainer to='/login'> */}
                             <Nav.Link as={Link} to='/login'><FaUser/>Sign In</Nav.Link>
